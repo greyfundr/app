8ca41c2c84206800bc5a36989f87a596bf5c9840
Jolomi Dudu <jollofdudu@gmail.com>
2025-11-29 22:25:43 +0100

    classes for editcampaign

--- lib/class/participants.dart
+++ lib/class/participants.dart
@@ -1,10 +1,23 @@
+// participants.dart
 class Participant {
   final int id;
   final String name;
   final String username;
   final String imageUrl;
 
-  Participant({required this.id, required this.name, required this.username, required this.imageUrl});
+  Participant({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.imageUrl,
+  });
+
+  // ADD THIS COPY CONSTRUCTOR
+  Participant.from(Participant other)
+      : id = other.id,
+        name = other.name,
+        username = other.username,
+        imageUrl = other.imageUrl;
 
   Map<String, dynamic> toJson() {
     return {
